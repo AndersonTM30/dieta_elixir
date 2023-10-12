63698f49975c1007d4b25697188144708ec215c0
Anderson Ferreira <anderson@pop-os.localdomain>
2023-10-12 20:39:18 -0300

    feat: criando a função retornarTMBEmDefictCalorico

--- lib/dieta_elixir.ex
+++ lib/dieta_elixir.ex
@@ -13,13 +13,13 @@ defmodule DietaElixir do
   """
   def metabolismoBasal(peso, altura, idade) do
     # IO.puts("Peso: #{peso} - Altura: #{altura} - Idade: #{idade}")
-    formulaPeso =  13.8 * peso
+    formulaPeso = 13.8 * peso
     # IO.puts("Form Peso: #{formulaPeso}")
     formulaAltura = 5 * altura
     # IO.puts("Form Altura: #{formulaAltura}")
     formulaIdade = 6.8 * idade
     # IO.puts("Form Idade: #{formulaIdade}")
-    _resultado =  (66 + (formulaPeso + formulaAltura)) - formulaIdade |> Float.ceil(2)
+    _resultado = (66 + (formulaPeso + formulaAltura) - formulaIdade) |> Float.ceil(2)
   end
 
   @doc """
@@ -43,10 +43,10 @@ defmodule DietaElixir do
 
   """
   def retornaPorcentagem(porcentagem) do
-    _resultado = porcentagem / 100 |> Float.ceil(2)
+    _resultado = (porcentagem / 100) |> Float.ceil(2)
   end
 
-    @doc """
+  @doc """
 
   ## Example
 
@@ -56,6 +56,20 @@ defmodule DietaElixir do
   """
   def retornarDefcitCalorico(tmb, porcentagem) do
     porcentagem = DietaElixir.retornaPorcentagem(porcentagem)
-    _converterPorcentagemEmNumero = tmb * porcentagem |> Float.ceil(2)
+    _converterPorcentagemEmNumero = (tmb * porcentagem) |> Float.ceil(2)
+  end
+
+  @doc """
+
+  ## Example
+
+      iex> DietaElixir.retornarTMBEmDefictCalorico(1864.4, 391.53)
+      1472.88
+
+  """
+  def retornarTMBEmDefictCalorico(tmb, defcitCalorico) do
+    taxaTMB = tmb
+    taxaDefcit = defcitCalorico
+    _resultado = (taxaTMB - taxaDefcit) |> Float.ceil(2)
   end
 end
